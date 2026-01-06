753b84c898f40c67b225ce7bf5e81d6391fd0668
chickencuber <n1batman728@gmail.com>
2026-01-06 10:41:01 -0600

    make screenshots work better

--- .config/hypr/screenshot.sh
+++ .config/hypr/screenshot.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
+output="$HOME/Pictures/Screenshots/"
+mkdir -p "$output"
 
-choice=$(echo -e "Active Monitor Screenshot\nActive Window Screenshot\nMonitor Screenshot\nWindow Screenshot\nRegion Screenshot" | wofi --dmenu --prompt "Choose:")
-sleep 0.1
-
-case "$choice" in
-    "Active Window Screenshot") hyprshot -m active --mode window;;
-    "Active Monitor Screenshot") hyprshot -m active --mode output;;
-    "Monitor Screenshot") hyprshot -m output;;
-    "Window Screenshot") hyprshot -m window ;;
-    "Region Screenshot") hyprshot -m region ;;
-    *) exit ;;
-esac
+if [[ "$1" == "normal" ]]; then
+    hyprshot -m active --mode output -o "$output"
+else
+    choice=$(echo -e "Active Monitor Screenshot\nActive Window Screenshot\nMonitor Screenshot\nWindow Screenshot\nRegion Screenshot" | wofi --dmenu --prompt "Choose:")
+    sleep 0.1
+    case "$choice" in
+        "Active Window Screenshot") hyprshot -m active --mode window -o "$output";;
+        "Active Monitor Screenshot") hyprshot -m active --mode output -o "$output";;
+        "Monitor Screenshot") hyprshot -m output -o "$output";;
+        "Window Screenshot") hyprshot -m window -o "$output";;
+        "Region Screenshot") hyprshot -m region -o "$output";;
+        *) exit ;;
+    esac
+fi
