219cb505f5456ac5b1a87eaa25cce5fdda868b09
chickencuber <n1batman728@gmail.com>
2026-01-10 20:32:40 -0600

    undid thing

--- .config/nvim/lua/main/remap.lua
+++ .config/nvim/lua/main/remap.lua
@@ -24,7 +24,7 @@ vim.keymap.set("n", "<leader>f", "mzggVG=`z")
 vim.keymap.set("n", "<leader>y", "mzggVGy`z")
 vim.keymap.set("n", "<leader>d", "mzggVGd`z")
 vim.keymap.set('n', '<leader>pp', 'ggVG"_dp')
-vim.keymap.set("n", "<leader>s", [[mz:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>=`z]])
+vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
 vim.keymap.set("n", "<leader>%", "ggVG")
