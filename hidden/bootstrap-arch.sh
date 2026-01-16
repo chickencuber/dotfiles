f5238cb82cb7be5448376c49047bca3edabff63e
chickencuber <n1batman728@gmail.com>
2026-01-15 19:31:56 -0600

    did thing

--- hidden/bootstrap-arch.sh
+++ hidden/bootstrap-arch.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 old=$(pwd)
 cd ~
+sudo pacman -Sy
 sudo pacman -S --needed stow git base-devel
 yay || {
     git clone https://aur.archlinux.org/yay.git
@@ -9,6 +10,9 @@ yay || {
 }
 cd "$old"
 stow .
-yay -S --needed - < "./hidden/packagesarch.txt"
+while IFS= read -r pkg; do
+    echo "Installing $pkg..."
+    yay -S --needed "$pkg"
+done < "./hidden/packagesarch.txt"
 
 
