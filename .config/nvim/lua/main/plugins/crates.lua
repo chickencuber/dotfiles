31ae579f4a21a23497ab4229c256ad50762e1952
chickencuber <n1batman728@gmail.com>
2026-01-22 11:21:00 -0600

    did thing

--- .config/nvim/lua/main/plugins/crates.lua
+++ .config/nvim/lua/main/plugins/crates.lua
@@ -2,6 +2,20 @@ return {
     'saecki/crates.nvim',
     tag = 'stable',
     config = function()
-        require('crates').setup()
+        require('crates').setup({
+            lsp = {
+                enabled = true,
+                actions = true,
+                completion = true,
+                hover = true,
+            },
+            completion = {
+                crates = {
+                    enabled = true, -- disabled by default
+                    max_results = 8, -- The maximum number of search results to display
+                    min_chars = 3, -- The minimum number of charaters to type before completions begin appearing
+                }
+            }
+        })
     end,
 }
