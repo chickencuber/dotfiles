e27b7587523f3529672b6a51be9f3b8174947fe1
chickencuber <n1batman728@gmail.com>
2026-01-27 13:12:45 -0600

    Re enabled bufferline

--- .config/nvim/lua/main/plugins/bufferline.lua
+++ .config/nvim/lua/main/plugins/bufferline.lua
@@ -2,7 +2,6 @@ return {
     'akinsho/bufferline.nvim',
     version = "*",
     dependencies = 'nvim-tree/nvim-web-devicons',
-    enabled = false,
     config=function()
         require("bufferline").setup {
             options = {
