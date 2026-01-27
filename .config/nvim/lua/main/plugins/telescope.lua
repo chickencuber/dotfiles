2428f5d5803bf25bd8e642f9e26988b76aae9fc2
chickencuber <n1batman728@gmail.com>
2026-01-27 12:03:30 -0600

    fixed thing

--- .config/nvim/lua/main/plugins/telescope.lua
+++ .config/nvim/lua/main/plugins/telescope.lua
@@ -6,6 +6,9 @@ return {
 
         require('telescope').setup{
             defaults = {
+                preview = {
+                    treesitter = false,
+                },
                 vimgrep_arguments = {
                     'rg',
                     '--color=never',
@@ -20,12 +23,9 @@ return {
 
         vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
         vim.keymap.set('n', '<C-p>', builtin.git_files, {})
-        vim.keymap.set('n', "<leader>pc", function ()
-            vim.cmd("Telescope buffers")
-        end)
         vim.keymap.set('n', '<leader>ps', function()
             builtin.grep_string({search = vim.fn.input("Grep > ")})
         end)
-        vim.keymap.set('n', '<leader>bf', builtin.buffers, { desc = 'Fuzzy find buffers' })
+        vim.keymap.set('n', '<leader>pb', builtin.buffers, { desc = 'Fuzzy find buffers' })
     end
 }
