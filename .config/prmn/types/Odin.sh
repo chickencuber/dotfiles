afdcbb0465f11d42c8237d838915a8bbde2c84e9
chickencuber <n1batman728@gmail.com>
2026-02-19 10:02:36 -0600

    did thing

--- .config/prmn/types/Odin.sh
+++ .config/prmn/types/Odin.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+mkdir ./build
 echo 'package main
 
 import "core:fmt"
@@ -6,4 +7,14 @@ import "core:fmt"
 main :: proc() {
 	fmt.println("Hello World!")
 }' > main.odin
+
+cat > justfile << 'EOF'
+build:
+    odin build main.odin -file -out:build/main
+run:
+    odin run main.odin -file
+EOF
+cat > .gitignore << 'EOF'
+/build
+EOF
 git init
