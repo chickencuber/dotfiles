14c215627b896f1203330c75f2b071b66caa31af
chickencuber <n1batman728@gmail.com>
2026-04-06 20:19:23 -0500

    fixed thing

--- .config/hypr/generate.sh
+++ .config/hypr/generate.sh
@@ -4,7 +4,7 @@ path="$HOME/.config/hypr/generated/$ACTIVE_CLASS.conf"
 if [[ -f "$path" ]] then
     rm "$path"
 else
-    echo "windowrule = hyprbars:no_bar true, match:class $ACTIVE_CLASS">>"$path"
+    echo "windowrule = hyprbars:no_bar true, match:class $ACTIVE_CLASS, match:float 1">>"$path"
 fi
 hyprctl reload
 
