eff4e4bf35592c6ff3f5c697f46ed2adcdfae5af
chickencuber <n1batman728@gmail.com>
2026-05-18 14:21:11 -0500

    updated install script

--- hidden/bootstrap-arch.sh
+++ hidden/bootstrap-arch.sh
@@ -13,15 +13,6 @@ cd "$old"
 stow .
 ./hidden/install-packages-arch.sh
 yes | yay -Scc
-# ly
-sudo mkdir -p /etc/ly
-if [ -f "/etc/ly/config.ini" ]; then
-    sudo rm -f /etc/ly/config.ini
-fi
-sudo cp ./hidden/lyconfig.ini /etc/ly/config.ini
-sudo chown root:root /etc/ly/config.ini
-sudo chmod 644 /etc/ly/config.ini
-sudo systemctl enable ly@tty1.service
 # plymouth 
 sudo mkdir -p /usr/share/plymouth/themes/
 tar -xzf ./hidden/catppuccin-mocha-plymouth.tar.gz -C /usr/share/plymouth/themes/
