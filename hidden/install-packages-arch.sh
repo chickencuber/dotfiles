e2b2d62dbc88f2f38190995dc3d5524e19d460d8
chickencuber <n1batman728@gmail.com>
2026-06-05 22:11:05 -0500

    did a thing

--- hidden/install-packages-arch.sh
+++ hidden/install-packages-arch.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+echo "installing system packages"
 failed=()
 w=$(wc -l< "./hidden/packagesarch.txt")
 l=1
@@ -17,3 +18,22 @@ else
     done
 fi
 
+
+echo "installing cargo packages"
+failed=()
+w=$(wc -l< "./hidden/packages-cargo.txt")
+l=1
+while IFS= read -r pkg; do
+    echo "Installing $pkg [$l/$w]..."
+    cargo install "$pkg" </dev/tty || failed+=("$pkg")
+    ((l++))
+done < "./hidden/packages-cargo.txt"
+echo
+if [ ${#failed[@]} -eq 0 ]; then
+    echo "All packages installed successfully."
+else
+    echo "===== PACKAGES THAT NEED MANUAL INSTALL ====="
+    for pkg in "${failed[@]}"; do
+        echo "$pkg"
+    done
+fi
